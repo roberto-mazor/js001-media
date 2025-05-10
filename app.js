b06abdca7541ee2e9d7930be2938e9462b709fe8
roberto-mazor <mazor.estudo@gmail.com>
2025-05-09 22:29:09 -0300

    Primeiro desafio
    Dependendo da nota, mostrar se a aluna ou aluno foi aprovada(o) ou não

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let n1 = 7, n2 = 5.5, n3 = 2, n4 = 4
 let media = (n1 + n2 + n3 + n4) / 4
 console.log("A média é: " + media.toFixed(2))
+
+if(media >= 7) {
+    console.log(Aprovado)
+}else {
+    console.log("Reprovado")
+}
